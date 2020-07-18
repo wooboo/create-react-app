8d704acf523eea23c07c5ad743fe7a883f05a5b4
Piotr Żabówka <wooboox@gmail.com>
2020-07-18 18:36:59 +0200

    tailwind integration

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <button class="btn btn-blue">Hello</button>
       </header>
     </div>
   );
